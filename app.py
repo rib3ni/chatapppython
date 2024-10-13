8cde657de7d8f45f39e07009d3f3a45f28b4e9cb
rib3ni <84444030+rib3ni@users.noreply.github.com>
2024-10-13 14:09:27 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
